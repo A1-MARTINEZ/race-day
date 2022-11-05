b21c4e4eb9938aa4b6c8f50775185c6f86bb7020
A1-MARTINEZ <116705619+A1-MARTINEZ@users.noreply.github.com>
2022-11-04 20:03:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+// RACE DAY
 
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const earlyBird = false;
+const age = 17;
+
+if (age >= 18 && earlyBird === true){
+    console.log(raceNumber + 1000)
+}
+if (age >= 18 && earlyBird === true){
+    console.log('You will run at 09:30am')
+}
+if (age < 18){
+    console.log(raceNumber + 1000)
+    console.log('You will run at 12:30pm')
+}
+else if (age >= 18 && earlyBird === false){
+    console.log(raceNumber + 1000)
+    console.log('You will run at 11:00am')
+}
